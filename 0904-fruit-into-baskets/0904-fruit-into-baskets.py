a99bf2b35b32b0a2b2df089374eb20eaf4771836
Gauri <gauri.256789@gmail.com>
2026-07-27 19:34:27 +0530

    Time: 180 ms (72.38%), Space: 26.2 MB (13.23%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        low=0 
+        max_len = 0
+        freq={}
+        for high in range(len(fruits)):
+            freq[fruits[high]] = freq.get(fruits[high],0)+1
+        
+       #shrink window if distict character > 2
+            while len(freq)>2:
+                freq[fruits[low]] -=1
+                if freq[fruits[low]] == 0:
+                    del freq[fruits[low]]
+                low +=1 
+        
+            max_len = max(max_len,high-low+1)
+            
+        return max_len
